c48e4f58777ecb50016fa00e75b0f89d1a37ee57
kul321 <kul321@naver.com>
2021-05-24 22:54:04 +0900

    마이페이지 ui 수정 & 장바구니까지 페이지연결

--- src/main/java/com/one/whospet/service/mypage/impl/MypageServiceImpl.java
+++ src/main/java/com/one/whospet/service/mypage/impl/MypageServiceImpl.java
@@ -1,8 +1,11 @@
 package com.one.whospet.service.mypage.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.one.whospet.dto.User;
 import com.one.whospet.service.mypage.face.MypageService;
 
+@Service
 public class MypageServiceImpl implements MypageService {
 
 	@Override
