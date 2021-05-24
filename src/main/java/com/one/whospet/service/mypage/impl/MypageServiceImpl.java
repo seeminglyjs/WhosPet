21ecd1f9edac7a3db74acbf60b9ba5424495de40
kul321 <kul321@naver.com>
2021-05-24 22:52:13 +0900

    ui 수정 & 장바구니까지 페이지연결

--- src/main/java/com/one/whospet/service/mypage/impl/MypageServiceImpl.java
+++ src/main/java/com/one/whospet/service/mypage/impl/MypageServiceImpl.java
@@ -1,7 +1,13 @@
 package com.one.whospet.service.mypage.impl;
 
+import com.one.whospet.dto.User;
 import com.one.whospet.service.mypage.face.MypageService;
 
 public class MypageServiceImpl implements MypageService {
+
+	@Override
+	public User getUserInfo(int uNo) {
+		return null;
+	}
 
 }
